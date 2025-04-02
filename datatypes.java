83769cf4497c4016ea1fa3b781677bdc9f39a385
Qurbanlii <105965054+Qurbanlii@users.noreply.github.com>
2025-04-02 18:43:04 +0400

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,10 @@
+public class datatypes {
+    
+    public static void main(String[] args) {
+        double number = 12.5;
+        number = -129;
+        System.out.println("salam");
+        char symbol = 'A';
+        boolean ifTrue = false;
+    }
+}
