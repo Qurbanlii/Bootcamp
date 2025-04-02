a5350d50d05af2eb4fad1fa7e4ce81893d10ca34
Ali Qurbanli <aliqurbanli2007@gmail.com>
2025-04-02 18:11:10 +0400

    elave edildi

---
+++ JAVA/datatypes.java
@@ -0,0 +1,11 @@
+public class datatypes {
+    
+    public static void main(String[] args) {
+        double number = 12.5;
+        number = -129;
+
+        char symbol = 'A';
+
+        boolean ifTrue = false;
+    }
+}
---
+++ JAVA/variables.java
@@ -0,0 +1,6 @@
+public class variables {
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println("Number:" + number);
+    }
+}
