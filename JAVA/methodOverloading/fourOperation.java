9a8dfde2ef73d36f8ba6710127042742847e075b
Ali Qurbanli <aliqurbanli2007@gmail.com>
2025-04-05 12:47:30 +0400

    Elave edildi

---
+++ JAVA/methodOverloading/fourOperation.java
@@ -0,0 +1,11 @@
+package methodOverloading;
+
+public class fourOperation {
+    public int addition(int number1, int number2){
+        return number1+number2;
+    }
+
+    public int addition2(int number1, int number2,int number3){
+        return number1+number2+number3;
+}
+}
---
+++ JAVA/methodOverloading/main3.java
@@ -0,0 +1,10 @@
+package methodOverloading;
+
+public class main3 {
+    
+    public main3(String[] args) {
+        fourOperation fourOperation = new fourOperation();
+        System.out.println(fourOperation.addition(2, 3));
+        fourOperation.addition2(2, 3, 5);
+    }
+}
