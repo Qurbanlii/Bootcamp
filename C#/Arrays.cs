b852f89a6a39660ac740ce1166e55ce08e973351
Ali Qurbanli <aliqurbanli2007@gmail.com>
2025-04-13 16:26:28 +0400

    Elave edildi

---
+++ C#/Arrays.cs
@@ -0,0 +1,25 @@
+namespace Arrays
+{
+  class Program
+{
+    static void Main(string[] args)
+    {
+        string[] student = new string[3]{"Ali" ,"Huseyn", "Mehdi"};
+
+        string[] student2 = {"Ali" ,"Huseyn", "Mehdi"};
+        student2[3] = "Murad";
+
+        foreach (var student in students2)
+        {
+            Console.WriteLine(student);
+        }
+
+        string[,] regions = new string [5,3];
+        regions[0,0]
+
+        Console.WriteLine();
+        Console.ReadLine();
+
+    }
+}  
+}
